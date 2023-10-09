82e4187996590b9e70e4166a1ad9f9f985f56dce
tdelgio <tomas.dg@me.com>
2023-10-09 08:07:21 -1000

    change order

--- src/partials/ProjectList.tsx
+++ src/partials/ProjectList.tsx
@@ -16,6 +16,20 @@ const ProjectList = () => (
   >
     <div className="flex flex-col gap-6 ">
       <Project
+        name="CompartIr"
+        description="Powered by Firebase for real-time updates and React for a smooth, interactive interface, CompartIr revolutionizes how we travel together. Whether you're planning a road trip or a daily commute, the app simplifies the process, promoting sustainability and community connections."
+        link="https://test.compartir.viajes"
+        img={{ src: "/assets/images/project-maps.png", alt: "Project Fire" }}
+        category={
+          <>
+            <Tags color={ColorTags.VIOLET}>React</Tags>
+            <Tags color={ColorTags.EMERALD}>Firebase</Tags>
+            <Tags color={ColorTags.YELLOW}>JavaScript</Tags>
+            <Tags color={ColorTags.RED}>MaterialUI</Tags>
+          </>
+        }
+      />
+      <Project
         name="SpaceX Launch Viewer"
         description="Dive into the future of space exploration with my latest project, the SpaceX Launch Viewer. Using cutting-edge technologies like Astro for lightning-fast performance and Tailwind CSS for sleek design, this web application fetches real-time data from SpaceX's API to display the most recent rocket launches."
         link="https://github.com/tdelgio/astro-tailwind"
@@ -29,20 +43,6 @@ const ProjectList = () => (
             <Tags color={ColorTags.LIME}>Web</Tags>
             <Tags color={ColorTags.SKY}>Tailwind.css</Tags>
             <Tags color={ColorTags.ROSE}>TypeScript</Tags>
-          </>
-        }
-      />
-      <Project
-        name="CompartIr"
-        description="Powered by Firebase for real-time updates and React for a smooth, interactive interface, CompartIr revolutionizes how we travel together. Whether you're planning a road trip or a daily commute, the app simplifies the process, promoting sustainability and community connections."
-        link="https://test.compartir.viajes"
-        img={{ src: "/assets/images/project-maps.png", alt: "Project Fire" }}
-        category={
-          <>
-            <Tags color={ColorTags.VIOLET}>React</Tags>
-            <Tags color={ColorTags.EMERALD}>Firebase</Tags>
-            <Tags color={ColorTags.YELLOW}>JavaScript</Tags>
-            <Tags color={ColorTags.RED}>MaterialUI</Tags>
           </>
         }
       />
